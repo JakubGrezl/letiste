51631ec1e71bbf74b36e65d1955606a0c6e039a0
JakubGrezl <jakub.grezl@outlook.cz>
2023-05-18 08:24:54 +0200

    XD

--- script/coursel-control.js
+++ script/coursel-control.js
@@ -2,24 +2,30 @@ let courselContent = [
     {
         "src": "img/plane1.png",
         "alt": "Letadlo 1",
+        "label": "Lorem ipsum",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nunc eget aliquam ultricies, nunc nisl ultricies nunc, nec ultr",
         "isActive": true
     },
 
     {
         "src": "img/plane2.png",
         "alt": "Letadlo 2",
+        "label": "XD",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nunc eget aliquam ultricies, nunc nisl ultricies nunc, nec ultr",
         "isActive": false
     },
 
     {
         "src": "img/plane3.png",
         "alt": "Letadlo 3",
+        "label": "TVOJEMAMA",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nunc eget aliquam ultricies, nunc nisl ultricies nunc, nec ultr",
         "isActive": false
     }
 ]
 
 courselContent.forEach(element => {
-    $(".carousel-inner").append('<div class="carousel-item' + IsActive(element) + '"><img class="d-block w-100" src="'+ element.src + '" alt="'+element.alt+'"></div>');
+    $(".carousel-inner").append('<div class="carousel-item' + IsActive(element) + '"><img class="d-block w-100" src="'+ element.src +'" alt="'+element.alt+'"><div class="carousel-caption d-none d-md-block"><h5>'+element.label+'</h5><p>'+element.description+'</p></div>')
 });
 
 function IsActive(element){
@@ -30,6 +36,3 @@ function IsActive(element){
         return "";
     }
 }
-
-const carousel = new bootstrap.Carousel('#mainCarousel');
-
